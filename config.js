86e4281ef3a134cd318e63fd5e0212923a41e99a
Kaushalyasl <149307067+Kaushalyasl@users.noreply.github.com>
2024-12-24 17:00:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: 'YOUR SESSION ID',
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'YOUR GIT TOKEN' : process.env.GITHUB_AUTH_TOKEN,
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'YOUR GIT USER NAME' : process.env.GITHUB_USER_NAME,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? 'YOUR NUMBER' : process.env.OWNER_NUMBER
+SESSION_ID: 'HANSAMAL-MD=6VIlQRbT#ZtMphaj1INeosu2OmLSo1gtXWHDRpssG7a4YYwGnIaw',
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'mx03UGgviGIjsVlNE8aR0bHr1LOd1K2ncJuD' : process.env.GITHUB_AUTH_TOKEN,
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'kaushalyasl' : process.env.GITHUB_USER_NAME,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94750122621' : process.env.OWNER_NUMBER
 };
